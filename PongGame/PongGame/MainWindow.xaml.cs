c335644c608b6d94466d88f176b9006c0856332a
Alexandru Mihai <the_snyper06@yahoo.com>
2014-02-18 15:59:07 +0200

    aaa

---
+++ PongGame/PongGame/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PongGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal readonly KinectSensorChooser sensorChooser;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            sensorChooser = new KinectSensorChooser();
+            SensorChooserUi.KinectSensorChooser = sensorChooser;
+            SensorChooserUi.KinectSensorChooser.KinectChanged += SensorChooserKinectChanged;
+            sensorChooser.Start();
+        }
+
+        private void SensorChooserKinectChanged(object sender, KinectChangedEventArgs e)
+        {
+            KinectSensor oldSensor = e.OldSensor;
+            KinectSensor newSensor = e.NewSensor;
+
+            if (oldSensor != null)
+            {
+                oldSensor.DepthStream.Disable();
+                oldSensor.SkeletonStream.Disable();
+            }
+        }
+    }
+}
